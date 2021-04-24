6efeb8ab6facbad5dd2afaab3baa9ce4ea678d71
Miguel Ángel Fernández Martínez <7636034@alumnos.ufv.es>
2021-04-24 12:48:06 +0200

    Comprobar

--- Django/PFG/PFG/settings.py
+++ Django/PFG/PFG/settings.py
@@ -125,6 +125,8 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
 
+STATICFILES_STORAGE = ' whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 LOGIN_REDIRECT_URL = '/home/'
 LOGOUT_REDIRECT_URL = '/login/'
 
